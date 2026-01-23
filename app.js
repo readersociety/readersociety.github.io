f0a2f60354a39cc8ff0ca36ac148d55087f1bc69
readersociety <portalsendgame@proton.me>
2026-01-23 13:51:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 import { songs } from "./data.js";
 import { loadPlayStats } from "./storage.js";
 import { renderSongList } from "./library.js";
+import { initNavigation, initSidebarToggle } from "./navigation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadPlayStats(songs);
 
-  const libraryContainer = document.querySelector(".song-list");
+  const libraryContainer = document.querySelector("#library .song-list");
   renderSongList(libraryContainer, songs);
+
+  initNavigation();
+  initSidebarToggle();
 });
